a9537affde0ee544190e6de573661129f7f5ec51
Odyssefs Aristeidou <airoddyisback@gmail.com>
2017-07-30 21:38:59 +0200

    - Angular Components Tutorial For Beginners - Components Exercise Solution Inside

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { SearchBoxComponent } from './search-box/search-box.component';
 import { ColorPickerComponent } from './color-picker/color-picker.component';
 import { ColorPreviewerComponent } from './color-previewer/color-previewer.component';
+import { ColorSampleComponent } from './color-sample/color-sample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchBoxComponent,
     ColorPickerComponent,
-    ColorPreviewerComponent
+    ColorPreviewerComponent,
+    ColorSampleComponent
   ],
   imports: [
     BrowserModule
